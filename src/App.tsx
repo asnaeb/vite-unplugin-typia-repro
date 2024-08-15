e5564a45ad3e8299201e736222a20b31b981bbb2
Roberto De Lucia <roberto.delucia@sinnefo.eu>
2024-08-15 04:57:22 +0200

    Clarified logs

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,27 @@ import {MyType} from "src/types/mytype";
 import {MyType2} from "@/types2/mytype2";
 import {MyType3} from "./types3/mytype3";
 
-console.log("Import from \"src/types/mytype\", should be false ->", typia.is<MyType>(null));
-console.log("Import from \"@/types2/mytype2\", should be false ->", typia.is<MyType2>(null));
-console.log("Import from \"./types3/mytype3\", should be false ->", typia.is<MyType3>(null));
+console.log(
+  "Path relative to baseUrl: import from \"src/types/mytype\", expected to be",
+  false,
+  "while returned",
+  typia.is<MyType>(null),
+  "<- Bad result"
+);
+console.log(
+  "Path mapped from tsconfig.compilerOptions.paths: import from \"@/types2/mytype2\", expected to be",
+  false,
+  "while returned",
+  typia.is<MyType2>(null),
+  "<- Bad result"
+);
+console.log(
+  "Relative path: import from \"./types3/mytype3\", expected to be",
+  false,
+  "while returned",
+  typia.is<MyType3>(null),
+  "<- Good result"
+);
 
 function App() {
   const [count, setCount] = useState(0)
