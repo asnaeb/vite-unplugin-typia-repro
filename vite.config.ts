07fc9681ec25ae9117b5427d13ac9c03eb0c5c69
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-17 10:46:02 +0100

    add vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnpluginTypia from '@ryoppippi/unplugin-typia/vite';
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Inspect(),
     UnpluginTypia({
       tsconfig: "./tsconfig.app.json",
       log: "verbose",
